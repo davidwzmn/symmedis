4ec5a7d97c10febabf42707343dec70f09de5f53
davidwzmn <c.waizmann@icloud.com>
2026-08-20 08:54:03 +0200

    Install frontend operational telemetry

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { installOperationalTelemetry } from './lib/operationalTelemetry.js'
 import './index.css'
+
+installOperationalTelemetry()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
